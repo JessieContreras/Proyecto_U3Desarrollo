128b7796da2756876e48728faa14d1f65bc36f4a
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-08-31 22:18:47 -0500

    Update IDetalleFacturaDAO.java

--- src/main/java/com/espe/dao/IDetalleFacturaDAO.java
+++ src/main/java/com/espe/dao/IDetalleFacturaDAO.java
@@ -5,12 +5,10 @@ import com.espe.model.DetalleFactura;
 import java.util.List;
 
 public interface IDetalleFacturaDAO {
-
     void guardar(DetalleFactura detalleFactura);
     void editar(DetalleFactura detalleFactura);
     DetalleFactura buscar(int id);
     List<DetalleFactura> obtenerDetalleFacturas();
     List<DetalleFactura> obtenerDetalleFacturas(int factura);
     void eliminar(int id);
-
 }
