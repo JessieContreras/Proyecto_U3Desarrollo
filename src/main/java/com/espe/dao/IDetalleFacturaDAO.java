9dce98f32bd4a70f44a4ef49248486757614ae82
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:42:41 -0500

    actualización de la clase IDetalleFacturaDAO

--- src/main/java/com/espe/dao/IDetalleFacturaDAO.java
+++ src/main/java/com/espe/dao/IDetalleFacturaDAO.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public interface IDetalleFacturaDAO {
 
-
+    void guardar(DetalleFactura detalleFactura);
+    void editar(DetalleFactura detalleFactura);
+    DetalleFactura buscar(int id);
+    List<DetalleFactura> obtenerDetalleFacturas();
+    List<DetalleFactura> obtenerDetalleFacturas(int factura);
+    void eliminar(int id);
 
 }
