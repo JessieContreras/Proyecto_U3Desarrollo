7b9c64e63989aa43daf45ccc85d0d5ff1b833d16
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 20:54:20 -0500

    editar en factura

--- src/main/java/com/espe/controller/FacturaBean.java
+++ src/main/java/com/espe/controller/FacturaBean.java
@@ -20,7 +20,18 @@ public class FacturaBean {
     public List<Factura> obtenerFacturas(){
         return facturaDAO.obtenerFacturas();
     }
+    public String editar(int id){
 
+        Factura oFactura;
+        oFactura = facturaDAO.buscar(id);
+
+        //crear una coleccion de tipo map
+        Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+        //pasar el objeto por medio del SessionMap hacia la vista
+        sessionMap.put("factura", oFactura);
+        System.out.println(oFactura);
+        return "./editar.xhtml";
+    }
 
 
 
