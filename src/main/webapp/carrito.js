26b631c0ad81f3b4b934fc0cde914fcb93f6edef
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-08-31 21:37:11 -0500

    Update carrito.js

--- src/main/webapp/carrito.js
+++ src/main/webapp/carrito.js
@@ -3,15 +3,7 @@ var contenidoCarrito = "<div>Contenido del carrito...</div>";
 
 function mostrarCarrito() {
     
-    var modal = document.createElement("div");
-    modal.className = "carrito-modal";
-    modal.innerHTML = contenidoCarrito;
-    document.body.appendChild(modal);
-    modal.addEventListener("click", function(event) {
-        if (event.target === modal) {
-            document.body.removeChild(modal);
-        }
-    });
+   
 }
 
 function redirectToPage(role) {
