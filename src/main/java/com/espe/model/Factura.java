5212b9a9b6f247294c9bb72a76642f6ffa39f085
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-09-01 01:12:04 -0500

    Update Factura.java

--- src/main/java/com/espe/model/Factura.java
+++ src/main/java/com/espe/model/Factura.java
@@ -23,35 +23,5 @@ public class Factura {
     // Getters y setters
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Long usuario) {
-        this.usuario = usuario;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
+    
 }
