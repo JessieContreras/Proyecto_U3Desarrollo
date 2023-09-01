3ee9b025c5bbf6578e67d51e689c6a9294522951
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:45:12 -0500

    actualización de la clase IPlatoDAO

--- src/main/java/com/espe/dao/IPlatoDAO.java
+++ src/main/java/com/espe/dao/IPlatoDAO.java
@@ -5,6 +5,11 @@ import com.espe.model.Plato;
 import java.util.List;
 
 public interface IPlatoDAO {
-
+     void guardar(Plato plato);
+    void editar(Plato plato);
+    Plato buscar(int id);
+    List<Plato> obtenerPlatos();
+    void eliminar(int id);
+    List<Plato> buscarPorCategoria(int id);
     
 }
