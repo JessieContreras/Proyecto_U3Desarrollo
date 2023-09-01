f4b62555c1ddd051ee9b35b5d1182f67c02556b1
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:15:52 -0500

    Clase DetalleFactura

--- src/main/java/com/espe/idao/DetalleFacturaDAOImpl.java
+++ src/main/java/com/espe/idao/DetalleFacturaDAOImpl.java
@@ -18,47 +18,4 @@ public class DetalleFacturaDAOImpl implements IDetalleFacturaDAO {
         //JPAUtil.shutdown();
     }
 
-    @Override
-    public void editar(DetalleFactura detalleDetalleFactura) {
-        entityManager.getTransaction().begin();
-        entityManager.merge(detalleDetalleFactura);
-        entityManager.getTransaction().commit();
-        ///JPAUtil.shutdown();
-    }
-
-    @Override
-    public DetalleFactura buscar(int id) {
-        DetalleFactura oDetalleFactura = new DetalleFactura();
-        oDetalleFactura = entityManager.find(DetalleFactura.class, id);
-        //JPAUtil.shutdown();
-        return oDetalleFactura;
-    }
-
-    @Override
-    public List<DetalleFactura> obtenerDetalleFacturas() {
-        List<DetalleFactura> listaDetalleFacturas;
-        //sentencia JPQL
-        Query query = entityManager.createQuery("SELECT u FROM DetalleFactura u");
-        listaDetalleFacturas = query.getResultList();
-        return listaDetalleFacturas;
-    }
-
-    @Override
-    public void eliminar(int id) {
-        DetalleFactura oDetalleFactura;
-        oDetalleFactura = entityManager.find(DetalleFactura.class, id);
-        entityManager.getTransaction().begin();
-        entityManager.remove(oDetalleFactura);
-        entityManager.getTransaction().commit();
-    }
-    @Override
-    public List<DetalleFactura> obtenerDetalleFacturas(int factura) {
-        List<DetalleFactura> listaDetalleFacturas;
-        //sentencia JPQL
-        Query query = entityManager.createQuery("SELECT u FROM DetalleFactura u where u.factura=:factura");
-        query.setParameter("factura",factura);
-        listaDetalleFacturas = query.getResultList();
-        return listaDetalleFacturas;
-    }
-
-}
+ 
