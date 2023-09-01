a5104dafe57bb15c37c67338f080b6d1236b1aed
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-09-01 01:13:28 -0500

    Update DetalleFactura.java
    actualizar

--- src/main/java/com/espe/model/DetalleFactura.java
+++ src/main/java/com/espe/model/DetalleFactura.java
@@ -21,9 +21,44 @@ public class DetalleFactura {
         super();
     }
 
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    
+    public Long getFactura() {
+        return factura;
+    }
 
-    // Otros métodos y lógica
+    public void setFactura(Long factura) {
+        this.factura = factura;
+    }
+
+    public Long getPlato() {
+        return plato;
+    }
+
+    public void setPlato(Long plato) {
+        this.plato = plato;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
 }
