58c06ca25039dea56645dbb3ab3509609db87a17
JennifferGomez <66042785+JennifferGomez@users.noreply.github.com>
2023-08-31 20:24:49 -0500

    Update carrito.js

--- src/main/webapp/carrito.js
+++ src/main/webapp/carrito.js
@@ -5,7 +5,6 @@ function mostrarCarrito() {
     var modal = document.createElement("div");
     modal.className = "carrito-modal";
     modal.innerHTML = contenidoCarrito;
-    
     document.body.appendChild(modal);
 
 
