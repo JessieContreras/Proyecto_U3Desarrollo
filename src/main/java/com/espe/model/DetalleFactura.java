918b2d42f61c9a24c528dac5866f2f9f16f3347c
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 20:37:33 -0500

    Actualizacion del modelo DetalleFactura

--- src/main/java/com/espe/model/DetalleFactura.java
+++ src/main/java/com/espe/model/DetalleFactura.java
@@ -8,6 +8,14 @@ public class DetalleFactura {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
+    private Long factura;
+
+    @Column
+    private Long plato;
+
+    private int cantidad;
+    private double subtotal;
 
     public DetalleFactura() {
         super();
