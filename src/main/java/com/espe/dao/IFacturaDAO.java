6503596416a42aa9cf439c3f116e16eed9ca122b
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-08-31 22:18:33 -0500

    Update IFacturaDAO.java
    Actualizacion

--- src/main/java/com/espe/dao/IFacturaDAO.java
+++ src/main/java/com/espe/dao/IFacturaDAO.java
@@ -5,7 +5,6 @@ import com.espe.model.Factura;
 import java.util.List;
 
 public interface IFacturaDAO {
-  
     Factura guardar(Factura factura);
     void editar(Factura factura);
     Factura buscar(int id);
