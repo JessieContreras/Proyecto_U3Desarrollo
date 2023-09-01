21a4ba695cd7afa06459baca9d2d5c479979c735
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:38:21 -0500

    editar usuario

--- src/main/java/com/espe/idao/UsuarioDAOImpl.java
+++ src/main/java/com/espe/idao/UsuarioDAOImpl.java
@@ -17,5 +17,12 @@ public class UsuarioDAOImpl implements IUsuarioDAO {
         entityManager.getTransaction().commit();
         //JPAUtil.shutdown();
     }
+     @Override
+    public void editar(Usuario usuario) {
+        entityManager.getTransaction().begin();
+        entityManager.merge(usuario);
+        entityManager.getTransaction().commit();
+        ///JPAUtil.shutdown();
+    }
 
    
