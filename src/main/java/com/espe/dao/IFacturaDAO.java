f23f38dae128ac26c4aa4c9c65a9207ea509be05
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:43:37 -0500

    actualización de la clase IFacturaDAO

--- src/main/java/com/espe/dao/IFacturaDAO.java
+++ src/main/java/com/espe/dao/IFacturaDAO.java
@@ -5,5 +5,11 @@ import com.espe.model.Factura;
 import java.util.List;
 
 public interface IFacturaDAO {
-
+  
+    Factura guardar(Factura factura);
+    void editar(Factura factura);
+    Factura buscar(int id);
+    List<Factura> obtenerFacturas();
+    void eliminar(int id);
+    Factura buscarLogin(String username, String password);
 }
