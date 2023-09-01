84b0e4989c62803b5cf5f8bea74076d3993a4531
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-09-01 01:11:41 -0500

    Update ItemCarrito.java

--- src/main/java/com/espe/model/ItemCarrito.java
+++ src/main/java/com/espe/model/ItemCarrito.java
@@ -14,6 +14,23 @@ public class ItemCarrito {
     public void aumentarCantidad() {
         cantidad++;
     }
+    public double getTotal() {
+        return plato.getPrecio() * cantidad;
+    }
 
-    
+    public Plato getPlato() {
+        return plato;
+    }
+
+    public void setPlato(Plato plato) {
+        this.plato = plato;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
