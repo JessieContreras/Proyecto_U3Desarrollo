502e0ad755680e80a87f72abeaa833c45a1aed32
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:46:36 -0500

    actualización de la clase IUsuarioDAO

--- src/main/java/com/espe/dao/IUsuarioDAO.java
+++ src/main/java/com/espe/dao/IUsuarioDAO.java
@@ -6,5 +6,11 @@ import java.util.List;
 
 public interface IUsuarioDAO {
 
+    void guardar(Usuario usuario);
+    void editar(Usuario usuario);
+    Usuario buscar(int id);
+    List<Usuario> obtenerUsuarios();
+    void eliminar(int id);
+    Usuario buscarLogin(String username, String password);
 
 }
