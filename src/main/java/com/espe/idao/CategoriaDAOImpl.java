e61d273267dc8387b4c62d244a2cccb1bbd1f3bc
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:12:55 -0500

    clase Categoria

--- src/main/java/com/espe/idao/CategoriaDAOImpl.java
+++ src/main/java/com/espe/idao/CategoriaDAOImpl.java
@@ -18,37 +18,5 @@ public class CategoriaDAOImpl implements ICategoriaDAO {
         //JPAUtil.shutdown();
     }
 
-    @Override
-    public void editar(Categoria categoria) {
-        entityManager.getTransaction().begin();
-        entityManager.merge(categoria);
-        entityManager.getTransaction().commit();
-        ///JPAUtil.shutdown();
-    }
 
-    @Override
-    public Categoria buscar(int id) {
-        Categoria oCategoria = new Categoria();
-        oCategoria = entityManager.find(Categoria.class, id);
-        //JPAUtil.shutdown();
-        return oCategoria;
-    }
-
-    @Override
-    public List<Categoria> obtenerCategorias() {
-        List<Categoria> listaCategorias;
-        //sentencia JPQL
-        Query query = entityManager.createQuery("SELECT u FROM Categoria u");
-        listaCategorias = query.getResultList();
-        return listaCategorias;
-    }
-
-    @Override
-    public void eliminar(int id) {
-        Categoria oCategoria;
-        oCategoria = entityManager.find(Categoria.class, id);
-        entityManager.getTransaction().begin();
-        entityManager.remove(oCategoria);
-        entityManager.getTransaction().commit();
-    }
 }
