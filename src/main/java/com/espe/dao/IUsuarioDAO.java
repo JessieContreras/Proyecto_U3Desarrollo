98df658643c79805104c5c333b103c718ff66f23
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:45:57 -0500

    clases IUsuarioDAO

--- src/main/java/com/espe/dao/IUsuarioDAO.java
+++ src/main/java/com/espe/dao/IUsuarioDAO.java
@@ -6,10 +6,5 @@ import java.util.List;
 
 public interface IUsuarioDAO {
 
-    void guardar(Usuario usuario);
-    void editar(Usuario usuario);
-    Usuario buscar(int id);
-    List<Usuario> obtenerUsuarios();
-    void eliminar(int id);
-    Usuario buscarLogin(String username, String password);
+
 }
