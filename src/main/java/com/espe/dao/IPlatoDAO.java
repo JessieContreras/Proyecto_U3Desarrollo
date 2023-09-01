ccb4eac915facec1cfb8756e9a6316cd301d3477
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:44:53 -0500

    clases IPlatoDAO

--- src/main/java/com/espe/dao/IPlatoDAO.java
+++ src/main/java/com/espe/dao/IPlatoDAO.java
@@ -6,10 +6,5 @@ import java.util.List;
 
 public interface IPlatoDAO {
 
-    void guardar(Plato plato);
-    void editar(Plato plato);
-    Plato buscar(int id);
-    List<Plato> obtenerPlatos();
-    void eliminar(int id);
-    List<Plato> buscarPorCategoria(int id);
+    
 }
