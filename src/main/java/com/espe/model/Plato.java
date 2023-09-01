3d7d715317ced03c0ef165852bb2ecc0adcfb48e
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-09-01 01:10:20 -0500

    Update Plato.java

--- src/main/java/com/espe/model/Plato.java
+++ src/main/java/com/espe/model/Plato.java
@@ -21,43 +21,5 @@ public class Plato {
     // Getters y setters
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public Long getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Long categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
+   
 }
