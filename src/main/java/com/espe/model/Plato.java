9d3a585be85eae003ba16275a1828388dbb7ee14
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 20:42:22 -0500

    Actualizaciones Plato

--- src/main/java/com/espe/model/Plato.java
+++ src/main/java/com/espe/model/Plato.java
@@ -7,7 +7,17 @@ import org.hibernate.annotations.Type;
 public class Plato {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-   
+    private Long id;
+    @Column
+    private String nombre;
+    @Column
+    private double precio;
+
+    @Column
+    private Long categoria;
+
+    @Column(name = "imagen",  columnDefinition = "LONGTEXT")
+    private String imagen;
     // Getters y setters
 
 
