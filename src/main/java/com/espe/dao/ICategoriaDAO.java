998aed5a659a61cf3d795cbe0b2b5cb0876a413d
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-08-31 21:41:33 -0500

    actualización de la clase ICategoriaDAO

--- src/main/java/com/espe/dao/ICategoriaDAO.java
+++ src/main/java/com/espe/dao/ICategoriaDAO.java
@@ -6,6 +6,10 @@ import com.espe.model.Usuario;
 import java.util.List;
 
 public interface ICategoriaDAO {
-
+    void guardar(Categoria usuario);
+    void editar(Categoria usuario);
+    Categoria buscar(int id);
+    List<Categoria> obtenerCategorias();
+    void eliminar(int id);
 
 }
