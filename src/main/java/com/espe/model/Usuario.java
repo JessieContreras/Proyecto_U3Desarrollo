4c80037c82b7bb37ceda8d8787777614576a1312
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-09-01 01:06:46 -0500

    Update Usuario.java
    get y set

--- src/main/java/com/espe/model/Usuario.java
+++ src/main/java/com/espe/model/Usuario.java
@@ -26,6 +26,59 @@ public class Usuario {
     @Column
     private String direccion; // Agregamos el campo para la direccion
     // Getters y setters
+    public Long getId() {
+        return id;
+    }
 
-    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }    
 }
