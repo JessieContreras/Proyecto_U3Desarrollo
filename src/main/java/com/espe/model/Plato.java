79e33fca0e27a3f58534cf03f0943d3e6d62af99
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-09-01 01:10:46 -0500

    Update Plato.java
    gett y sett

--- src/main/java/com/espe/model/Plato.java
+++ src/main/java/com/espe/model/Plato.java
@@ -19,7 +19,43 @@ public class Plato {
     @Column(name = "imagen",  columnDefinition = "LONGTEXT")
     private String imagen;
     // Getters y setters
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Long getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Long categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    } 
 }
